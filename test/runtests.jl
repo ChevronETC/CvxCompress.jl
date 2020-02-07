19b4fa26275cf8fb55e380114ad5729598d338df
samtkaplan <sam.kaplan@chevron.com>
2020-02-07 14:03:19 -0600

    Minimal test-set
    Add some basic tests.  We didn't do this in the past because the code
    here is covered in Wave.jl compression.  But, this feels like a more
    complete package with some minimal amount of testing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,23 @@
-include("test_CvxCompress.jl")
+using CvxCompress, Test
+
+@testset "CvxCompress, 2D" begin
+    nz,nx,bz,bx,σ = 2048,4096,32,32,1e-3
+    x = rand(Float32, nz, nx)
+    y = zeros(Float32, nz, nx)
+    z = zeros(UInt32, nz*nx)
+    n = CvxCompress.compress!(z, CvxCompressor((bz,bx),σ), x)
+    CvxCompress.decompress!(y, CvxCompressor((bz,bx),σ), z, n)
+
+    @test x ≈ y rtol=1e-3
+end
+
+@testset "CvxCompress, 3D" begin
+    nz,ny,nx,bz,by,bx,σ = 128,64,256,32,32,32,1e-3
+    x = rand(Float32, nz, ny, nx)
+    y = zeros(Float32, nz, ny, nx)
+    z = zeros(UInt32, nz*ny*nx)
+    n = CvxCompress.compress!(z, CvxCompressor((bz,by,bx),σ), x)
+    CvxCompress.decompress!(y, CvxCompressor((bz,by,bx),σ), z, n)
+
+    @test x ≈ y rtol=1e-3
+end
--- test/test_CvxCompress.jl
+++
@@ -1,9 +0,0 @@
-using CvxCompress, Test, BenchmarkTools
-
-@testset "CvxCompress, 2D copy" begin
-    bz,bx,scale = 32, 16, 1e-3
-    cvx = CvxCompressor((bz,bx),scale)
-    cvx_copy = copy(cvx)
-    @test cvx_copy.br == cvx.br
-    @test cvx_copy.scale == cvx.scale
-end
